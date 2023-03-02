44c0abffd8e738020b8a90abe9a7fa0d7c224ed6
= <=>
2023-03-02 12:27:24 -0600

    Initial Commit

---
+++ src/firstclass/loopDemo.java
@@ -0,0 +1,11 @@
+package firstclass;
+
+public class loopDemo {
+    public static void main(String[] args){
+        int number = 10;
+        while(number<=10) {
+            System.out.println(number);
+            number = number--;
+        }
+    }
+}
